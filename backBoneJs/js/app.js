6b6700d3b3d0369e376478bbaec1399f1c12c778
Kato Takashi <taka4.kato@gmail.com>
2013-08-09 12:55:29 +0900

    #05 validateを使ってみよう

--- backBoneJs/js/app.js
+++ backBoneJs/js/app.js
@@ -4,6 +4,11 @@
 	 	title:'do it!',
 			completed: false
 		}, 
+		validate:function(attrs){
+			if(_.isEmpty(attrs.title)){
+				return "title must not be empty";
+			}
+		},
 		toggle:function(){
 			this.set('completed', !this.get('completed'));
 		}
@@ -12,8 +17,10 @@
 		completed: true
 	});
 	//task1.set('title', 'newTitle');
-	console.log(task1.toJSON());
-	task1.toggle();
+	//console.log(task1.toJSON());
+	//task1.toggle();
+	
+	task1.set({title: ''}, {validate: true});
 	console.log(task1.toJSON());
 	
 })();
