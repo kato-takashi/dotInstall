ad64986d910219bdc635c641d48f67b90e22671a
Kato Takashi <taka4.kato@gmail.com>
2013-08-10 00:19:10 +0900

    #15 taskの削除処理を作っていこう minだとなぜかエラーが出たため，デモファイルからコピー

--- backBoneJs/js/app.js
+++ backBoneJs/js/app.js
@@ -1,55 +1,60 @@
-(function(){
-	var Task = Backbone.Model.extend({
-	 defaults:{
-	 	title:'do something!',
-			completed: false
-		}
-	});
-	var task = new Task();
-	
-	////////////View
-	var TaskView = Backbone.View.extend({
-		tagName:'li',
-		
-		template: _.template($('#task-template').html()),
-		render: function(){
-			var template = this.template(this.model.toJSON());
-			this.$el.html(template);
-			return this;
-		}
-	});
-	////////////collection
-	var Tasks = Backbone.Collection.extend({
-		model:Task
-	});
-	var TasksView = Backbone.View.extend({
-		tagName:'ul',
-		render:function(){
-			this.collection.each(function(task){
-			var taskView = new TaskView({model:task});
-			this.$el.append(taskView.render().el);
-			}, this);
-			return this;
-		} 
-	});
-	var tasks = new Tasks([
-		{
-			title:'task1',
-			completed:true
-		},
-		{
-		title:'task2'
-		},
-		{
-		title:'task3'
-		}
-	]);
-	//console.log(tasks.toJSON());
-	var tasksView = new TasksView({collection:tasks});
-	$('#tasks').html(tasksView.render().el);
-	
-	
+(function() {
+
+var Task = Backbone.Model.extend({
+    defaults: {
+        title: 'do something',
+        completed: false
+    }
+});
+var Tasks = Backbone.Collection.extend({ model: Task });
+
+var TaskView = Backbone.View.extend({
+    tagName: 'li',
+	initialize: function(){
+		this.model.on('destroy', this.remove, this);
+	},
+	events:{'click .delete':'destroy'},
+	destroy: function(){
+		if(confirm('Are you shure?')){
+				this.model.destroy();
+			}
+	},
+	remove: function(){
+		this.$el.remove();
+	},
+    template: _.template($('#task-template').html()),
+    render: function() {
+        var template = this.template(this.model.toJSON());
+        this.$el.html(template);
+        return this;
+    }
+});
+var TasksView = Backbone.View.extend({
+    tagName: 'ul',
+    render: function() {
+        this.collection.each(function(task) {
+            var taskView = new TaskView({model: task});
+            this.$el.append(taskView.render().el);
+        }, this);
+        return this;
+    }
+});
+
+var tasks = new Tasks([
+    {
+        title: 'task1',
+        completed: true
+    },
+    {
+        title: 'task2'
+    },
+    {
+        title: 'task3'
+    }
+]);
+
+var tasksView = new TasksView({collection: tasks});
+$('#tasks').html(tasksView.render().el);
+
+
 })();
-
-
-
