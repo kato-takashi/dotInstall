37b9f98e4529b50ae08fff47cdf0de3fe0cb36f2
Takashi Kato <taka4.kato@gmail.com>
2013-08-26 08:32:06 +0900

    #09 ユーザー情報を取得しよう (2)  facebookでlogin! sessionの有効期限、フォルダ修正

--- login/fb_login/common/config.php
+++ login/fb_login/common/config.php
@@ -9,13 +9,11 @@ require_once($url);
 define('DB_HOST', 'localhost');
 define('DB_USER', $usrName);
 define('DB_PASSWORD', $pass);
-define('CONSUMER_KEY', $C_KEY);
-define('CONSUMER_SECRET', $C_SECRET);
+define('APP_ID', $C_KEY);
+define('APP_SECRET', $C_SECRET);
 
 define('SITE_URL', 'http://localhost/test/dotinstall/fb_login/fb_login/');
 
 error_reporting(E_ALL & ~E_NOTICE);
 
-session_set_cookie_params(0, '/fb_login/');
-//var_dump($C_KEY);
-//exit;
+session_set_cookie_params(0, '/test/dotinstall/fb_login/fb_login/');
--- login/fb_login/redirect.php
+++ login/fb_login/redirect.php
@@ -16,21 +16,83 @@ require_once('common/htmlESC.php');
 require_once('common/config.php');
 
 session_start();
+if (empty($_GET['code'])) {
+    // 認証の準備
+    
+    $_SESSION['state'] = sha1(uniqid(mt_rand(), true));
+    $params = array(
+        'client_id' => APP_ID,
+        'redirect_uri' => SITE_URL.'redirect.php',
+        'state' => $_SESSION['state'],
+        'scope' => 'user_birthday,user_website,friends_website, friends_birthday'
+    );
+    
+    $url = "https://www.facebook.com/dialog/oauth?".http_build_query($params);    
+    
+    // facebookに一旦飛ばす
+    header('Location: '.$url);
+    exit;
 
-if(empty($_GET['code'])){
-	//認証の準備
-	$_SESSIOTN['state'] = sha1(uniqid(at_rand(), true));
-	$params = array(
-		'client_id'=> SITE_URL.'redirect.php', 
-		'state'=> $_SESSION['state'], 
-		'scope'=> 'user_website, friends_website'
-	);
-	$url = "https://www.facebook.com/dialog/oauth?".http_build_query($params);
-	// facebookに一旦飛ばす
-}else{
-	//認証後の処理
-	// ユーザー情報の取得
-	// 	DB処理
-	// 	ログイン処理
-	// 	index.php
+} else {
+    // 認証後の処理
+    // CSRF対策
+    if ($_SESSION['state'] != $_GET['state']) {
+        echo "不正な処理！";
+        exit;
+    }
+    
+    // ユーザー情報の取得
+    $params = array(
+        'client_id' => APP_ID,
+        'client_secret' => APP_SECRET,
+        'code' => $_GET['code'],
+        'redirect_uri' => SITE_URL.'redirect.php'
+    );
+    $url = 'https://graph.facebook.com/oauth/access_token?'.http_build_query($params);    
+    $body = file_get_contents($url);
+    parse_str($body);
+    
+    $url = 'https://graph.facebook.com/me?access_token='.$access_token.'&fields=name,picture,birthday,email,bio';
+    $me = json_decode(file_get_contents($url));
+    var_dump($me);
+    exit;
+
+    // DB処理
+    // DB処理
+    try {
+        $dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASSWORD);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+    
+    $stmt = $dbh->prepare("select * from users where facebook_user_id=:user_id limit 1");
+    $stmt->execute(array(":user_id"=>$me->id));
+    $user = $stmt->fetch();
+
+    if (empty($user)) {
+        $stmt = $dbh->prepare("insert into users (facebook_user_id, facebook_name, facebook_birthday,facebook_picture, facebook_access_token, created, modified) values (:user_id, :name,:birthday, :picture, :access_token, now(), now());");
+        $params = array(
+            ":user_id"=>$me->id,
+            ":name"=>$me->name,
+            ":birthday"=>$me->birthday,
+            ":picture"=>$me->picture->data->url,
+            ":access_token"=>$access_token
+        );
+        $stmt->execute($params);
+        $stmt = $dbh->prepare("select * from users where id=:last_insert_id limit 1");
+        $stmt->execute(array(":last_insert_id"=>$dbh->lastInsertId()));
+        $user = $stmt->fetch();        
+    }
+     //var_dump($user);
+     //exit;
+    
+    // ログイン処理
+    if (!empty($user)) {
+        session_regenerate_id(true);
+        $_SESSION['user'] = $user;
+    }
+    
+    // index.php
+    header('Location: '.SITE_URL);
 }
