d6dcfcb48fe0c9e135daf674d41b9f7bf5ad5bda
Kato Takashi <taka4.kato@gmail.com>
2013-12-02 08:23:43 +0900

    #08 AxisHelperを使おう three.js

--- threeJS/works/js/ex.js
+++ threeJS/works/js/ex.js
@@ -7,13 +7,28 @@
 	var scene = new THREE.Scene();
 
 	//mesh
+	//cube
 	var geometry = new THREE.CubeGeometry(50, 50, 50);
-	var material = new THREE.MeshBasicMaterial({color: "#ff0000"});
+	//var material = new THREE.MeshBasicMaterial({color: "#ff0000"});
+	var material = new THREE.MeshLambertMaterial({color: "#ff0000"});
 	var cube = new THREE.Mesh(geometry, material);
 	cube.position.set(0, 0, 0); // rotate, scaleができる
 	scene.add(cube);
 
+	//sphere
+	var sGeometry = new THREE.SphereGeometry(30);
+	//var material = new THREE.MeshBasicMaterial({color: "#ff0000"});
+	var sMaterial = new THREE.MeshLambertMaterial({color: "#ffff00"});
+	var sphere = new THREE.Mesh(sGeometry, sMaterial);
+	sphere.position.set(100, 100, 100); // rotate, scaleができる
+	scene.add(sphere);
+
 	//light
+	var light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 100, 30);
+    scene.add(light);
+    var ambient = new THREE.AmbientLight(0x550000);
+    scene.add(ambient);
 	
 	//camera
 	var camera = new THREE.PerspectiveCamera(45, width/height, 1, 1000);
@@ -22,6 +37,11 @@
 	//camera.position = new THREE.Vector3(0, 0, 0);
 	camera.position.set(200, 200, 500);
 	camera.lookAt(cube.position);
+
+	//helper
+	var axis = new THREE.AxisHelper(1000);
+	axis.position.set(0, 0, 0);
+	scene.add(axis);
 
 	//rendering
 	var renderer = new THREE.WebGLRenderer();
