3256ca2f6e1b7f0f533b70431545dc6d2a7a3638
Kato Takashi <taka4.kato@gmail.com>
2013-12-27 22:48:56 +0900

    dotinstall nodeJS #5webサーバーを作ってみよう

---
+++ nodeJS/server.js
@@ -0,0 +1,9 @@
+var http =require("http");
+var server = http.createServer();
+server.on('request', function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('hello world');
+	res.end();
+});
+server.listen(8124);
+console.log("server listening....");
