e85c81fbf50be5b24376d11d2d5efdd37b33330c
Takashi Kato <taka4.kato@gmail.com>
2014-01-17 08:42:32 +0900

    06設定を外部ファイルに移す　nodeJS

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,9 +1,11 @@
 var http =require("http");
+var settings = require('./settings.js');
+console.log(settings);
 var server = http.createServer();
 server.on('request', function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('hello world');
+	res.write('hello world!!');
 	res.end();
 });
-server.listen(8124);
-console.log("server listening....");
+server.listen(settings.port, settings.host);
+console.log("server listening　http://127.0.0.1/....");
