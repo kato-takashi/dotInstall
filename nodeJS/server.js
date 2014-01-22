370e64bf6194eb48332f1753acce6584c8d6fb2c
Takashi Kato <taka4.kato@gmail.com>
2014-01-22 09:18:43 +0900

    #08 HTMLファイルを読み込んでみよう nodeJs

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,11 +1,21 @@
 var http =require("http");
+var	fs = require('fs');
 var settings = require('./settings.js');
 console.log(settings);
+var msg;
+
 var server = http.createServer();
 server.on('request', function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('hello world!!');
-	res.end();
+	fs.readFile('./public_html/hello.html', 'utf-8', function(err, data){
+		if(err){
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.write('not found');
+			return res.end();
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
 });
 server.listen(settings.port, settings.host);
-console.log("server listening　http://127.0.0.1/....");
+console.log("server listening　http://127.0.0.1:8124....");
