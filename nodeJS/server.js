cf1fc875e32a406b47baf7f614c6bba74c294696
Takashi Kato <taka4.kato@gmail.com>
2014-01-23 09:32:33 +0900

    #10 ejsを使ってページを表示してみよう node.js

--- nodeJS/server.js
+++ nodeJS/server.js
@@ -1,21 +1,21 @@
 var http =require("http");
 var	fs = require('fs');
+var ejs = require('ejs');
 var settings = require('./settings.js');
-console.log(settings);
-var msg;
-
 var server = http.createServer();
+var template = fs.readFileSync('./public_html/hello.ejs', 'utf-8');
+var n = 0;
+
 server.on('request', function(req, res){
-	fs.readFile('./public_html/hello.html', 'utf-8', function(err, data){
-		if(err){
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.write('not found');
-			return res.end();
-		}
+		n=n+1;
+		var data = ejs.render(template, {
+			title: "hello",
+			content: "<strong>World!</strong>",
+			n: n
+		});
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(data);
 		res.end();
-	});
 });
 server.listen(settings.port, settings.host);
 console.log("server listening　http://127.0.0.1:8124....");
