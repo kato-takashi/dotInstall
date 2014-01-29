7d6c1e22d1f20d2bb316ac2b6e5e41a0c8f1e120
Takashi Kato <taka4.kato@gmail.com>
2014-01-30 08:36:39 +0900

    #07	middlewareを作ってみよう express

--- express/myapp/app.js
+++ express/myapp/app.js
@@ -1,9 +1,15 @@
 var express = require('express'),
 	app = express();
+	//middleware
+app.use(express.logger('dev'));
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next){
+	console.log('my custom middleware nodemon!');
+	next();
+});
 app.get('/hello.txt', function(req, res){
-	res.sendfile(__dirname +'/public/hello.txt');	
+	res.send('hello from app.js');	
 	
 });
 
