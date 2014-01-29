0a54587d351ce0fc320f6d60d4662168824ee00f
Takashi Kato <taka4.kato@gmail.com>
2014-01-30 08:09:58 +0900

    #04	プレースホルダーを使おう

--- express/myapp/app.js
+++ express/myapp/app.js
@@ -1,11 +1,16 @@
 var express = require('express'),
 	app = express();
 app.use(app.router);
-app.get('/', function(req, res){
-	res.send("hello world");
+app.get('/users/:name?', function(req, res){
+	if(req.params.name){
+		res.send("hello!　"+req.params.name);
+	}else{
+		res.send("hello!　nobady");
+	}
+	
 });
-app.get('/about', function(req, res){
-	res.send("about this page!");
+app.get('/items/:id([0-9]+)', function(req, res){
+	res.send('item no: '+ req.params.id);
 });
 
 app.listen(3000);
