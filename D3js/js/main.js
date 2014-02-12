b74cbc755cc6afa5d5c9607b9b193f6dd676cd46
Takashi Kato <taka4.kato@gmail.com>
2014-02-13 08:35:44 +0900

    #04 styleを設定してみよう  D3.js

--- D3js/js/main.js
+++ D3js/js/main.js
@@ -1,5 +1,17 @@
 //D3.js
 // 1 要素を指定して　select selectAll
 // 2 設定したり、取得したり text arrt
-console.log("test");
-d3.select("body").selectAll("p").text("hello from d3!");
+/*
+var p = d3.select("body").selectAll("p");
+p.text("hello from d3!")
+	.style({
+		"font-size": "28px",
+		"font-weight": "bold"
+	});
+*/
+
+var p = d3.select("body").selectAll("p");
+p.style("font-size", function(){
+	return Math.floor(Math.random()*29 )+ "px";
+});
+//alert(p.style("font-size"));
