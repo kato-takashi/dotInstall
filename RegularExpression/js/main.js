40bfd00ad85be1cc099e6ae40359aaad9d35cd10
Kato Takashi <taka4.kato@gmail.com>
2014-02-16 10:47:18 +0900

    #09 フラグについて　@正規表現

--- RegularExpression/js/main.js
+++ RegularExpression/js/main.js
@@ -2,13 +2,27 @@
 /*　a? 0 or 1 -> あるかないか null or a
 a* 0 or more -> null or aaaa ....
 a+ 1 or more -> a, aaa, aaa....
+
+(abc)* -> abc, abcabc... null or abcabc
+ | -> or 
+
+\n -> 改行
+\t -> タブ
+\d ->　数字 [0-9]
+\w ->　英数字_ [A-Za-z0-9_]
+
+フラグ
+ i -> 大文字小文字を区別しない　tagchu TAGUCHI
+ g -> すべてのマッチした要素を配列で返す
+ m -> 複数業に対応させる　改行も含めてマッチ
 */
  
 var s ='@taguchi, @fkoji, @dotinstall';
-var rs = s.match(/@dotinstall$/);
+var rs = s.match(/a/g);
+
 
 if(rs){
-	console.log("マッチしました！");
+	console.log(rs.length);
 }else{
 	console.log("あれっ？");
 }
