91bb07f1aeba13c04b079c5061e7c0105f29ecd7
Kato Takashi <taka4.kato@gmail.com>
2014-02-16 10:16:37 +0900

    #10 データを使ってcircleを描画しよう @D3.js

--- D3js/js/main.js
+++ D3js/js/main.js
@@ -1,42 +1,25 @@
 //D3.js
 // 1 要素を指定して　select selectAll
 // 2 設定したり、取得したり text arrt
-/*
-var p = d3.select("body").selectAll("p");
-p.text("hello from d3!")
-	.style({
-		"font-size": "28px",
-		"font-weight": "bold"
-	});
-*/
-// var dataset = [12, 24, 36, 48];
-var dataset = [12, 24];
-var p = d3.select("body").selectAll("p");
-/*dataをつかった瞬間に仮想領域が作られる　->
-update: 対応する要素がある場合 -> text, style
-enter:　対応する要素が足りない場合 -> append
-exit:　対応する要素が余った場合 -> remove
 
-*/
+var dataset = [11, 25, 45, 30, 33];
+var w = 500;
+var h = 200;
 
-var update  = p.data(dataset);
-var exit = update.exit();
 
-update.text(function(d){
-	return "update: "+ d;
-});
+var num1 = 50;
+var num2 = 100;
 
-exit.style("color", "red");
 
-/*var enter = update.enter();
-
-update.text(function(d){
-	return "update: "+ d;
-});
-
-enter.append('p').text(function(d){
-	return "enter: " +d
-});*/
-
-//d3.select("body").append('p').text("hello from  append").remove();
-//alert(p.style("font-size"));
+var svg = d3.select("body").append("svg").attr({width:w, height:h});
+svg.selectAll("circle")
+			.data(dataset)
+			.enter()
+			.append("circle")
+			.attr({
+				cx: function(d, i){return num1 + (i* num2);},
+				cy: h/2,
+				r: function(d){return d},
+				fill:"red"
+			});
+			
