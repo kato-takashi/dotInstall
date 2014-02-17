d7abfd73ad0bb9a6b99b020799bd62b97c2643ee
Takashi Kato <taka4.kato@gmail.com>
2014-02-18 08:23:36 +0900

    #17 axisを使ってみよう (2) @D3.js

--- D3js/js/main.js
+++ D3js/js/main.js
@@ -2,36 +2,46 @@
 // 1 要素を指定して　select selectAll
 // 2 設定したり、取得したり text arrt
 
-var dataset = [11, 25, 45, 30, 33];
+var dataset = [11, 25, 52, 30, 33];
 var w = 500;
 var h = 200;
+var padding = 20;
 
 var num1 = 20;
 var num2 = 100;
 
 var dMin = 0;
 var dMax = 50;
+var svg = d3.select("body").append("svg").attr({width:w, height:h});
+
 
 // スケールの定義
 var xScale = d3.scale.linear()
 			.domain([dMin, d3.max(dataset)])
-			.range([0, w])
+			.range([padding, w-padding])
 			.nice(); //nice() キリの良い数字にしてくれるよ
 
+// 軸の定義
+var xAxis = d3.svg.axis()
+				.scale(xScale)
+				.orient("bottom"); //数字の向き
 
 
-
-
-var svg = d3.select("body").append("svg").attr({width:w, height:h});
+svg.append("g")
+	.attr({
+		class: "axis",
+		transform: "translate(0, 180)"
+	})
+	.call(xAxis);
 
 svg.selectAll("rect")
 	.data(dataset)
 	.enter()
 	.append('rect')
 	.attr({
-		x:0,
+		x:padding,
 		y: function(d, i){return i*25;},
-		width: function(d){return xScale(d);},
+		width: function(d){return xScale(d)-padding;},
 		height:num1,
 		fill:"red"
 	});
