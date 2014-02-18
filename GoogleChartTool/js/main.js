15573ada69d80b30b6096d9906412c79039ea841
Takashi Kato <taka4.kato@gmail.com>
2014-02-19 08:25:39 +0900

    #06 横棒グラフを描画する@Google Chart Tools

--- GoogleChartTool/js/main.js
+++ GoogleChartTool/js/main.js
@@ -3,26 +3,29 @@ google.load("visualization", "1.0", {"packages": ["corechart"]});
 google.setOnLoadCallback(drawChart);
 
 function drawChart(){
-	// 円グラフ
+	// 棒グラフ
 	// 1.データを用意する
 	var data = new google.visualization.DataTable();
-	data.addColumn("string", "活動");
-	data.addColumn("number", "時間");
+	data.addColumn("string", "年");
+	data.addColumn("number", "売上");
+	data.addColumn("number", "利益");
 	data.addRows([
-			["睡眠", 8],
-			["仕事", 12],
-			["読書", 4]
+			["2010", 500, 300],
+			["2011", 600, 320],
+			["2012", 700, 200],
+			["2013", 800, 200],
+			["2014", 400, 120],
+			["2015", 200, 30],
 		]);
 
 	// 2.グラフのオプションを指定する
 	var options = {
-		title: "1日の内訳",
+		title: "業績推移",
 		width: 500,
 		height: 500,
-		pieSliceText: 'label',
-		is3D: true
+		isStacked: true
 	};
 	// 3.描画する
-	var chart = new google.visualization.PieChart(document.getElementById('chart'));
+	var chart = new google.visualization.BarChart(document.getElementById('chart'));
 	chart.draw(data, options);
 }
