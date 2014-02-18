379275b620c48482881e87fc789522b9bb5330fe
Takashi Kato <taka4.kato@gmail.com>
2014-02-19 08:56:28 +0900

    #09	Googleスプレッドシートと連動させる @Google Chart Tools

--- GoogleChartTool/js/main.js
+++ GoogleChartTool/js/main.js
@@ -3,29 +3,7 @@ google.load("visualization", "1.0", {"packages": ["corechart"]});
 google.setOnLoadCallback(drawChart);
 
 function drawChart(){
-	// 折れ線グラフ
-	// 1.データを用意する
-	var data = new google.visualization.DataTable();
-	data.addColumn("string", "年");
-	data.addColumn("number", "売上");
-	data.addColumn("number", "利益");
-	data.addRows([
-			["2010", 500, 300],
-			["2011", 600, 320],
-			["2012", 700, 200],
-			["2013", 800, 200],
-			["2014", 400, 120],
-			["2015", 200, 30],
-		]);
-
-	// 2.グラフのオプションを指定する
-	var options = {
-		title: "業績推移",
-		width: 500,
-		height: 500,
-		isStacked: true
-	};
-	// 3.描画する
-	var chart = new google.visualization.LineChart(document.getElementById('chart'));
-	chart.draw(data, options);
+	// スプレッドシートから
+	// 
+	//https://docs.google.com/spreadsheet/pub?key=0AgjGJjZ8WS2gdEFWOXgxZW82akJqMl9VVV9LTjMtbGc&output=html
 }
