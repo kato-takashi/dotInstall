04b8ec56b73f2006f2add83f41dc768b9b6fc072
Kato Takashi <taka4.kato@gmail.com>
2014-02-19 22:55:48 +0900

    #07 最後の仕上げをしよう @javascript スロットマシーン

--- Let'Smake/JS_slot/js/main.js
+++ Let'Smake/JS_slot/js/main.js
@@ -2,6 +2,17 @@ var timers = [];
 var nums = [];
 var stopCount = 0;
 
+document.getElementById("stop0").onclick = function(){
+		stopSlot(0);
+	};
+document.getElementById("stop1").onclick = function(){
+		stopSlot(1);
+	};
+document.getElementById("stop2").onclick = function(){
+		stopSlot(2);
+	};
+
+
 (function startSlot(){
 	//	alert("test")
 	runSlot(0);
@@ -10,13 +21,16 @@ var stopCount = 0;
 })();
 
 function runSlot(n){
-	document.getElementById("num"+n).innerHTML = Math.floor(Math.random()*3);
+	document.getElementById("num"+n).innerHTML = Math.floor(Math.random()*10);
 	timers[n] = setTimeout(function(){
 		runSlot(n);
 	}, 50);
 }
 
 function stopSlot(n){
+	if(typeof nums[n] !== "undefined"){
+		return;
+	}
 	clearTimeout(timers[n]);
 	nums[n] = document.getElementById("num"+n).innerHTML;
 	stopCount++;
@@ -32,9 +46,9 @@ function checkSlot(){
 	nums.sort();
 	if(nums[0] == nums[1] && nums[0] == nums[2]){
 		alert("全部そろったよ！");
-	}else if(nums[0] == nums[1] ||  nums[0] == nums[2]){
+	}else if(nums[0] == nums[1] || nums[1] == nums[2] ||  nums[0] == nums[2]){
 		alert("二つそろったよ！");
 	}else{
-		console.log("残念。");
+		alert("残念。");
 	}
 }
