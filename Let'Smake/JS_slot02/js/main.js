357b6af4db9cd835f26f9d25318ea7b8199e4233
Takashi Kato <taka4.kato@gmail.com>
2014-03-07 07:48:16 +0900

    スロット

---
+++ Let'Smake/JS_slot02/js/main.js
@@ -0,0 +1,44 @@
+var timers = [];
+var nums = [];
+var stopCount = 0;
+
+
+(function startSlot(){
+	//	alert("test")
+	runSlot(0);
+	runSlot(1);
+	runSlot(2);
+})();
+
+function runSlot(n){
+	document.getElementById("num"+n).innerHTML = Math.floor(Math.random()*10);
+	timers[n] = setTimeout(function(){
+		runSlot(n);
+	}, 50);
+}
+
+function stopSlot(n){
+	if(typeof nums[n] !== "undefined"){
+		return;
+	}
+	clearTimeout(timers[n]);
+	nums[n] = document.getElementById("num"+n).innerHTML;
+	stopCount++;
+
+	if(stopCount == 3){
+		checkSlot();
+		console.log(nums);
+	}
+}
+
+function checkSlot(){
+	// alert("check!");
+	nums.sort();
+	if(nums[0] == nums[1] && nums[0] == nums[2]){
+		alert("全部そろったよ！");
+	}else if(nums[0] == nums[1] || nums[1] == nums[2] ||  nums[0] == nums[2]){
+		alert("二つそろったよ！");
+	}else{
+		alert("残念。");
+	}
+}
