7a0b9c3b392bad199af98da9172a8a2c68eb9b19
Kato Takashi <taka4.kato@gmail.com>
2014-03-13 08:31:05 +0900

    #04 grunt-contrib-lessを使おう grunt

--- Grunt/Gruntfile.js
+++ Grunt/Gruntfile.js
@@ -2,10 +2,17 @@
 module.exports = function(grunt){
 	//config
 	grunt.initConfig({
-
+		less:{
+			build:{
+				src: 'src/style.less',
+				dest: 'build/style.css'
+			}
+		}
 	});
 
 	//plugin
-	
+	grunt.loadNpmTasks('grunt-contrib-less');
+
 	//tasks
+	grunt.registerTask('default', 'less');
 };
