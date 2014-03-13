dbd507dd6527cd1203e0bfd07d5e3026199eb191
Takashi Kato <taka4.kato@gmail.com>
2014-03-14 07:54:34 +0900

    #05 ターゲットを設定しよう grunt

--- Grunt/Gruntfile.js
+++ Grunt/Gruntfile.js
@@ -1,11 +1,23 @@
 //Gruntfile.js
 module.exports = function(grunt){
+	/*task:{
+		target1:{
+			設定
+		},
+		target2:{
+			設定
+		}
+	}*/
 	//config
 	grunt.initConfig({
 		less:{
-			build:{
-				src: 'src/style.less',
-				dest: 'build/style.css'
+			build1:{
+				src: 'src/style1.less',
+				dest: 'build/style1.css'
+			},
+			build2:{
+				src: 'src/style2.less',
+				dest: 'build/style2.css'
 			}
 		}
 	});
@@ -15,4 +27,6 @@ module.exports = function(grunt){
 
 	//tasks
 	grunt.registerTask('default', 'less');
+	grunt.registerTask('task1', 'less:build1');
+	grunt.registerTask('task2', 'less:build2');
 };
