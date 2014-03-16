5d28bc5772ca5a289a9ca68505c06976803d7f79
Kato Takashi <taka4.kato@gmail.com>
2014-03-16 19:27:12 +0900

    #10 grunt-contrib-cssmin, grunt-contrib-watchを使おう grunt

--- Grunt/Gruntfile.js
+++ Grunt/Gruntfile.js
@@ -1,6 +1,8 @@
 //Gruntfile.js
 module.exports = function(grunt){
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
 		less:{
 			build:{
 				/*src: 'src/style1.less',
@@ -9,20 +11,39 @@ module.exports = function(grunt){
 					dest:'build/style1.css' 				
 			}
 		},
+		
 		csslint: {
 			check:{
 				// src: 'build/style1.css' 
 				src: '<%= less.build.dest %>'
 			}
+		},
+
+		cssmin:{
+			minimize:{
+				options: {
+					banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd")%> */'
+				},
+			files:{
+					'build/styles.min.css' : '<%= less.build.dest %>'
+				}
+			}
+		},
+
+		watch:{
+			files: 'src/*.less',
+			tasks: ['less', 'csslint', 'cssmin']	
 		}
 	});
 
 	//plugin
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	//tasks
-	grunt.registerTask('default', ['less', 'csslint']);
+	grunt.registerTask('default', ['less', 'csslint', 'cssmin', 'watch']);
 	//grunt.registerTask('task1', 'less:build');
 	//grunt.registerTask('task2', 'less:build2');
 };
