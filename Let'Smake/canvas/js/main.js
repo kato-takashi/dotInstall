113a707a6adad9b771bc81539c87a4f95d2679a6
Takashi Kato <taka4.kato@gmail.com>
2014-04-03 08:23:45 +0900

    #04	線を描画していこう @Canvasでお絵かき

--- Let'Smake/canvas/js/main.js
+++ Let'Smake/canvas/js/main.js
@@ -1,7 +1,37 @@
 $(function(){
-	var canvas = document.getById("mycanvas");
+	var canvas = document.getElementById("mycanvas");
 	if(!canvas || !canvas.getContext){
 		return false;
 	}
 	var ctx = canvas.getContext('2d');
+	var startX,
+		startY,
+		x,
+		y,
+		borderWidth = 10,
+		isDrawing = false;
+
+	$('#mycanvas').mousedown(function(e){
+		isDrawing = true;
+		startX = e.pageX - $(this).offset().left - borderWidth;
+		startY = e.pageY - $(this).offset().top - borderWidth;
+	})
+	.mousemove(function(e){
+		if(!isDrawing) return;
+		x = e.pageX - $(this).offset().left - borderWidth;
+		y = e.pageY - $(this).offset().top - borderWidth;
+		ctx.beginPath();
+		ctx.moveTo(startX, startY);
+		ctx.lineTo(x, y);
+		ctx.stroke();
+		startX = x;
+		startY = y;
+
+	})
+	.mouseup(function(e) {
+		isDrawing = false;
+	})
+	.mouseleave(function(e) {
+		isDrawing = false;
+	});
 });
