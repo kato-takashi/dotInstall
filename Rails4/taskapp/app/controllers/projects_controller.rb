07bd8cf805ec029eb96d85c0ac84dc872ca7d030
Takashi Kato <taka4.kato@gmail.com>
2014-10-02 08:25:21 +0900

    #14 Validationを設定しよう rails4

--- Rails4/taskapp/app/controllers/projects_controller.rb
+++ Rails4/taskapp/app/controllers/projects_controller.rb
@@ -2,4 +2,26 @@ class ProjectsController < ApplicationController
   def index
   	@projects = Project.all
   end
+
+  def show
+    @project = Project.find(params[:id]) 
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projec_path
+    else
+        render 'new'
+    end
+   end
+
+  private
+   def project_params
+       params[:project].permit(:title)
+   end
 end
