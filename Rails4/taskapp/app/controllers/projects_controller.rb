671859f455d0090315064d205e420afed5dc2118
Takashi Kato <taka4.kato@gmail.com>
2014-10-10 08:26:40 +0900

    #18 データを削除しよう rails4

--- Rails4/taskapp/app/controllers/projects_controller.rb
+++ Rails4/taskapp/app/controllers/projects_controller.rb
@@ -32,8 +32,15 @@ class ProjectsController < ApplicationController
         render 'edit'
     end
   end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+
+  end
   private
-   def project_params
+  def project_params
        params[:project].permit(:title)
    end
 end
