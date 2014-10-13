209407ab0d262af464339890815f5ff41be8ff6a
Takashi Kato <taka4.kato@gmail.com>
2014-10-14 08:35:45 +0900

    #09 sort､reverse､split､joinを使おう python入門

---
+++ python/hello09.py
@@ -0,0 +1,13 @@
+# coding: UTF-8
+#リスト
+sales = [50, 100, 80, 45]
+# sort /reve
+#sales.sort()　#要素を小さい順にする
+#大きい順にしたいときは　1.sort() 2.reverse()
+#sales.reverse() #要素の順番を逆にする
+
+#文字列とリスト
+d = "2014/10/14"
+#print d.split("/")
+a = ["a", "b", "c"]
+print "-".join(a)
