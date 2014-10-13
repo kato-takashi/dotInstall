caecdb98ddce1ef013bd83b520a94e83850f00bc
Takashi Kato <taka4.kato@gmail.com>
2014-10-14 07:57:22 +0900

    before_actionを使ってみよう　rails4

--- Rails4/taskapp/app/controllers/projects_controller.rb
+++ Rails4/taskapp/app/controllers/projects_controller.rb
@@ -1,10 +1,11 @@
 class ProjectsController < ApplicationController
+  
+  before_action :set_project, only: [:show, :edit, :update, :destroy]  
   def index
   	@projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id]) 
   end
 
   def new
@@ -21,12 +22,10 @@ class ProjectsController < ApplicationController
    end
 
   def edit
-    @project = Project.find(params[:id])
-  end
+     end
 
   def update
-    @project = Project.find(params[:id])
-    if @project.update(project_params)
+       if @project.update(project_params)
         redirect_to projects_path
     else
         render 'edit'
@@ -34,7 +33,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path
 
@@ -43,4 +41,8 @@ class ProjectsController < ApplicationController
   def project_params
        params[:project].permit(:title)
    end
+
+  def set_project 
+    @project = Project.find(params[:id])
+  end
 end
